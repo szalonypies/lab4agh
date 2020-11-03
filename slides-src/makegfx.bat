12f369979c4a635c703bea1dedbcceee99399a98
Pawel Jaworski <pawel.j.jaworski@gmail.com>
2020-11-03 06:29:38 +0100

    Latex build Makefile for slides. Fixed verbatim issues by adding [fragile] to frames.

--- slides-src/makegfx.bat
+++ slides-src/makegfx.bat
@@ -1,2 +1,2 @@
 @echo off
-for /f %%f in ('dir /b *.svg') do ("c:\Program Files\Inkscape\inkscape.exe" -D -z --file=%%f --export-area-page --export-pdf=%%~nf.pdf & echo %%f done)
+for /f %%f in ('dir /b *.svg') do ("c:\Program Files\Inkscape\bin\inkscape.exe" -D -z --file=%%f --export-area-page --export-pdf=%%~nf.pdf & echo %%f done)
