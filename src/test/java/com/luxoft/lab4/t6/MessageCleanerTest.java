4ba003c20f8aa1b84644554c115b67bc19826361
Pawel Jaworski <pawel.j.jaworski@gmail.com>
2020-11-10 16:36:55 +0100

    slides update

--- src/test/java/com/luxoft/lab4/t6/MessageCleanerTest.java
+++ src/test/java/com/luxoft/lab4/t6/MessageCleanerTest.java
@@ -5,6 +5,9 @@ import com.luxoft.lab4.util.PerformanceTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.lang.management.MemoryType;
 import java.util.Random;
 
 /**
@@ -22,12 +25,13 @@ public class MessageCleanerTest {
         createIntegersAndPutThemInStore(1_000_000);
     }
 
-    /** Why the code requires more than 56M to run? How can we get lower on memory consumption?
-     * Which list uses more memory than others? */
-    @PerformanceTest(heapMaxMb = 56)
+    /** Try to decrease the heap below 42. it seems that 42 is the boundary right?
+     * Which list uses more memory than the other? */
+    @PerformanceTest(heapMaxMb = 50)
     @Test
     public void testMemEfficiencyEasy() throws Exception {
         createIntegersAndPutThemInStore(1_000_000);
+        printMemoryUsage();
     }
 
     /** Why the code requires more than 32M to run? How can we get lower on memory consumption? */
@@ -37,11 +41,12 @@ public class MessageCleanerTest {
         createIntegersAndPutThemInStore(1_000_000);
     }
 
-    /** Why the code requires more than 29M to run? To get so low we can go with Java's best suited collection */
-    @PerformanceTest(heapMaxMb = 29)
+    /** Why the code requires more than 26M to run? To get so low we can go with Java's best suited collection */
+    @PerformanceTest(heapMaxMb = 26)
     @Test
     public void testMemEfficiencyExpert() throws Exception {
         createIntegersAndPutThemInStore(1_000_000);
+        printMemoryUsage();
     }
 
     /** To go as low as 12M we need to craft a custom solution (we have already written it before) */
@@ -49,12 +54,13 @@ public class MessageCleanerTest {
     @Test
     public void testMemEfficiencyMaster() throws Exception {
         createIntegersAndPutThemInStore(1_000_000);
+        printMemoryUsage();
     }
 
     private void createIntegersAndPutThemInStore(int howManyIntegers) {
         int[] testIntegers = createTestIntegers(howManyIntegers);
 
-        MessagesIds messageIds = new MessageIdsListImpl();
+        MessagesIds messageIds = new MessageIdsListImpl(howManyIntegers);
 
         for (int i : testIntegers) {
             messageIds.addId(i);
@@ -92,4 +98,16 @@ public class MessageCleanerTest {
             }
         }
     }
+
+    private void printMemoryUsage() {
+        for (MemoryPoolMXBean mpBean: ManagementFactory.getMemoryPoolMXBeans()) {
+            if (mpBean.getType() == MemoryType.HEAP) {
+                System.out.printf(
+                        "Name: %s: %s\n",
+                        mpBean.getName(), mpBean.getUsage()
+                );
+            }
+        }
+    }
+
 }
